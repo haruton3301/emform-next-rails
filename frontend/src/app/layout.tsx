cdfd5c216ca537e3b3c37d9892e345464bcdcfc7
Haruto Ikehara <kduxwr@gmail.com>
2025-03-15 08:04:53 +0900

    Add msw

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { MswLoader } from "@/components/MswLoader"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <MswLoader />
         {children}
       </body>
     </html>
